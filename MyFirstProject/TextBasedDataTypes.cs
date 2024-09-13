1f3df3b6e95f795752152166a3af5207e1729cf6
CodeD <davidff2010@gmail.com>
2024-09-13 16:12:19 +0200

    TextBasedDataTypes

---
+++ MyFirstProject/TextBasedDataTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class TextBasedDataTypes
+    {
+        static void Main(string[] args)
+        {
+            string name = "David";
+            char character = 'a';
+            
+            Console.WriteLine(character);
+            Console.WriteLine();
+            Console.WriteLine("My name is");
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
