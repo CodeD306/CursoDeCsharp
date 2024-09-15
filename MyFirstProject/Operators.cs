4bd02e7450cf6ae337ddfa3f3a4f3a2c0c839745
CodeD <davidff2010@gmail.com>
2024-09-15 21:51:51 +0200

    Operators

--- MyFirstProject/Operators.cs
+++ MyFirstProject/Operators.cs
@@ -11,8 +11,31 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-           
+            int age = 14;
+            float x = 1;
+            string helloWorld = "Hello";
+
+            age = age + 1;
+            age += 1;
+            age++;
+            Console.WriteLine(age);
             
+            age = age - 1;
+            age -= 1;
+            age--;
+            Console.WriteLine(age);
+
+            x *= 5;
+            x /= 6;
+            Console.WriteLine(x);
+            x = 100;
+            Console.WriteLine(x % 90);
+
+            helloWorld += "World";
+            Console.WriteLine(helloWorld);
+
+
+
             Console.ReadLine();
         }
     }
