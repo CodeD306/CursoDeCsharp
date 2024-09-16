1717b9da1e2bc4fde97e82a135779479af683338
CodeD <davidff2010@gmail.com>
2024-09-16 14:44:57 +0200

    VarKeyboard

---
+++ MyFirstProject/VarKeyboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class VarKeyboard
+    {
+        static void Main(string[] args)
+        {
+           //Var asume que tipo de variable es la seleccionada
+            
+            var age = 5;
+            Console.WriteLine(age);
+
+            var bigNumber = 9000000l;
+            Console.WriteLine(bigNumber);
+
+            var name = "David";
+            Console.WriteLine(name);
+
+            //Solo debería usarse si es obvio su tipo.
+
+            Console.ReadLine();
+        }
+    }
+}
