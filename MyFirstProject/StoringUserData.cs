9d108f99033ccb63321f0b474ec9c28e6f5e65bf
CodeD <davidff2010@gmail.com>
2024-10-09 22:36:47 +0200

    StoringUserData

--- MyFirstProject/StoringUserData.cs
+++ MyFirstProject/StoringUserData.cs
@@ -10,8 +10,14 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            var name = "David";
+            var password = "012345";
+            var age = 14;
 
-
+            Console.WriteLine(name);
+            Console.WriteLine(password);
+            Console.WriteLine(age);
+                
             Console.ReadLine();
         }
     }
