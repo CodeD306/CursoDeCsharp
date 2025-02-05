f4b1957bd2817ddb9ef6d333aecce4d6428691a7
CodeD <davidff2010@gmail.com>
2025-02-05 21:13:47 +0100

    NumberGuesser

---
+++ MyFirstProject/NumberGuesser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class NumberGuesser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dime el primer número");
+            string firstNumberTxt = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumberTxt);
+
+            Console.WriteLine("Dime el segundo número");
+            string secondNumberTxt = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondNumberTxt);
+
+            int answer = firstNumber * secondNumber;
+
+            Console.WriteLine("¿Cúal es la respuesta de " + firstNumber + " x " + secondNumber);
+            string yourAsnwerTxt = Console.ReadLine();
+            int yourAsnwer = Convert.ToInt32(yourAsnwerTxt);
+
+
+            if (yourAsnwer == answer)
+            {
+                Console.WriteLine("Es correcto!");
+            }
+            else
+            {
+                Console.WriteLine("No es correcto :(");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
