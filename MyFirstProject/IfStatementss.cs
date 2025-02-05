9fe4f904f869cd52f44b284647b3cba1e1ed2add
CodeD <davidff2010@gmail.com>
2025-02-05 16:39:31 +0100

    IfStatements

--- MyFirstProject/IfStatementss.cs
+++ MyFirstProject/IfStatementss.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,31 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            
 
+            
+            Console.WriteLine("¿Cúantos años tienes?");
+            Console.WriteLine();
+
+            string textAge = Console.ReadLine();
+            int age = Convert.ToInt32(textAge);
+
+            
+            if (age <= 0 || age >= 150)
+            {
+                Console.WriteLine("Esa no puede ser tu edad");
+            }
+            else if (age >= 19)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else if (age == 17)
+            {
+                Console.WriteLine("You old bitch");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
             
             Console.ReadLine();
         }
