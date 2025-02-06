9b5d6e2f91d56c21bafa9f1c8255e59c06f4d581
CodeD <davidff2010@gmail.com>
2025-02-06 17:16:46 +0100

    SwitchStatements

--- MyFirstProject/SwitchStatements.cs
+++ MyFirstProject/SwitchStatements.cs
@@ -11,7 +11,37 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("elo");
+            Console.WriteLine("Dime un día de la semana usando los números del 1 al 7");
+            string weekDaysTxt = Console.ReadLine();
+            int weekDays = Convert.ToInt32(weekDaysTxt);
+
+            switch(weekDays) 
+            { 
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miércoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Dómingo");
+                    break;
+                default:
+                    Console.WriteLine("Resultado inválido");
+                    break;
+            }
 
             Console.ReadLine();
         }
