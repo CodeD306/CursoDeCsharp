c54c98c3801d35542af94fe5870a7e3782e8370b
CodeD <davidff2010@gmail.com>
2025-02-06 23:32:37 +0100

    ForLoops

---
+++ MyFirstProject/ForLoops.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ForLoops
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Que quieres escribir?");
+            string txt = Console.ReadLine();
+
+            Console.WriteLine("Cuántas veces quieres que se repita?");
+            string loopTxt = Console.ReadLine();
+            int loopNum = Convert.ToInt32(loopTxt);
+
+            for (int i = 0; i < loopNum; i++)
+            {
+                Console.WriteLine(txt);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
