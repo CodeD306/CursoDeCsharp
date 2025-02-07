6f546c2918dfa29cfd51da74febb6374a5fd9226
CodeD <davidff2010@gmail.com>
2025-02-07 17:54:00 +0100

    WhileLoops

--- MyFirstProject/WhileLoops.cs
+++ MyFirstProject/WhileLoops.cs
@@ -11,8 +11,35 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dime el primer número");
+            string firstNumberTxt = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumberTxt);
 
-            Console.WriteLine("helo");
+            Console.WriteLine("Dime el segundo número");
+            string secondNumberTxt = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondNumberTxt);
+
+            int answer = firstNumber * secondNumber;
+
+            Console.WriteLine("¿Cúal es la respuesta de " + firstNumber + " x " + secondNumber);
+            string yourAnswerTxt = Console.ReadLine();
+            int yourAnswer = Convert.ToInt32(yourAnswerTxt);
+
+            do
+            {
+
+                    Console.WriteLine("No es correcto :(");
+                    Console.WriteLine();
+                    Console.WriteLine("Intentalo denuevo:");
+                    yourAnswerTxt = Console.ReadLine();
+                    yourAnswer = Convert.ToInt32(yourAnswerTxt);
+
+          
+            } while (yourAnswer != answer);
+             {
+                Console.WriteLine("Es correcto!");
+             }
+
 
             Console.ReadLine();
         }
