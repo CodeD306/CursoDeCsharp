559e2fa56596e3d9725d6f2dec986e70a70855b0
CodeD306 <davidff2010@gmail.com>
2025-02-13 21:50:04 +0100

    TryParseFunctionFile

---
+++ MyFirstProject/TryParseFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class TryParseFunction
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("elo");
+
+            Console.ReadLine();
+        }
+    }
+}
