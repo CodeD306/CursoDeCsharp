1d2de7a42e504d587a70598482bf08115341d35a
CodeD306 <davidff2010@gmail.com>
2025-02-14 16:57:22 +0100

    TryParseFunctionFix

--- MyFirstProject/TryParseFunction.cs
+++ MyFirstProject/TryParseFunction.cs
@@ -17,15 +17,27 @@ namespace MyFirstProject
             string numTxt = Console.ReadLine();
 
             int num = 0;
+            bool correct = int.TryParse(numTxt, out num);
 
-            while (!int.TryParse(numTxt, out num))
+
+
+            while (!correct)
             {
                 Console.WriteLine();
                 Console.WriteLine("Está mal >:(");
                 Console.WriteLine();
                 Console.WriteLine("Dime un número");
                 numTxt = Console.ReadLine();
+                correct = int.TryParse(numTxt, out num);
             }
+
+            if (correct)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Si es " + num);
+            }
+
+            Console.ReadLine();
         }
     }
 }
