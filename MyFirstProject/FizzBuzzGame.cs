ab0df2ba4332a4fd15e6fb3e2eb0475870fe03ba
CodeD306 <davidff2010@gmail.com>
2025-02-15 17:05:57 +0100

    FizzBuzzGame

--- MyFirstProject/FizzBuzzGame.cs
+++ MyFirstProject/FizzBuzzGame.cs
@@ -13,7 +13,33 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
+            bool fiveDiv = false;
+            bool threeDiv = false;
             
+            
+            for (int i = 1; i <= 15; i++)
+            {
+                fiveDiv = i % 5 == 0;
+                threeDiv = i % 3 == 0;
+
+                if (threeDiv && fiveDiv)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (threeDiv)
+                
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (fiveDiv)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
             
 
             Console.ReadLine();
