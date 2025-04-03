5befd11dac04702e8ac0e492533cf5b90c46ed0e
CodeD <davidff2010@gmail.com>
2025-04-03 16:45:20 +0200

    PasswordCheckerFile

---
+++ MyFirstProject/PasswordChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class PasswordChecker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("helo");
+
+            Console.ReadLine();
+
+        }
+    }
+}
