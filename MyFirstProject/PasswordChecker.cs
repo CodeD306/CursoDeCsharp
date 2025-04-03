5c6c0efd614e270c7fe4b620910aad3f258e5727
CodeD <davidff2010@gmail.com>
2025-04-03 17:38:54 +0200

    PasswordChecker

--- MyFirstProject/PasswordChecker.cs
+++ MyFirstProject/PasswordChecker.cs
@@ -11,10 +11,60 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("helo");
+            string ogPassword = "";
+            string password = "";
+            int checker = 0;
 
-            Console.ReadLine();
 
+            while (checker == 0)
+            {
+                while (ogPassword.Equals(string.Empty) || password.Equals(string.Empty))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Write a password");
+                    ogPassword = Console.ReadLine();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Write the password again");
+                    password = Console.ReadLine();
+
+                    if (ogPassword.Equals(string.Empty) || password.Equals(string.Empty))
+                    {
+                        Console.WriteLine("Error, write the passwords again");
+                        Console.WriteLine();
+                    }
+                }
+
+                
+                if (ogPassword.Length < 6)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The passwords are not long enough, write them again");
+                    ogPassword = "";
+                    password = "";
+                }
+                
+                else if (ogPassword != password)
+                {
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("The passwords are not the same, write them again");
+                        ogPassword = "";
+                        password = "";
+                    }
+                }
+                
+                else if (ogPassword == password)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Both passwords are the same");
+                    checker = 1;
+                }
+                
+
+
+
+            }
         }
     }
 }
