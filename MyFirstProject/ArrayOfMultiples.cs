1b8a5d4e8ba0838092c30a50b3b376be8f7031a8
CodeD <davidff2010@gmail.com>
2025-04-08 16:51:13 +0200

    ArrayOfMultiples

---
+++ MyFirstProject/ArrayOfMultiples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class ArrayOfMultiples
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un número");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Escribe cuantas multiplicaciones quieres que aparezcan");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] array = new int[length];
+
+            for(int i = 0; i < array.Length; i++) 
+            {
+                int xNum = num * (i + 1);
+
+                array[i] = xNum;
+
+            }
+
+            foreach (int value in array) 
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
